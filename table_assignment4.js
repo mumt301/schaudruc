dffe83ff9f876a6abdfaf1c8f17687fc4cabc5a4
sofiachaudruc <71230015+sofiachaudruc@users.noreply.github.com>
2020-10-05 13:10:07 -0400

    Create table_assignment4.js

---
+++ table_assignment4.js
@@ -0,0 +1,15 @@
+function assignment_table() {
+    let table = document.createElement('table');
+    table.id = "residual_table"
+    for (row = 0; row < assignment4_table.length; row++) {
+        tr = document.createElement('tr');
+        let table_data = Object.keys(assignment4_table[row]);
+        for (col = 0; col < Object.keys(assignment4_table[row]).length; col++) {
+            td = document.createElement('td');
+            tr.appendChild(td);
+            td.innerHTML = assignment4_table[row][table_data[col]];
+        }
+        table.appendChild(tr);
+    }
+    document.getElementById("final_table").appendChild(table);
+}
